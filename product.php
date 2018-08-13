697d4b8c6d0a2fd8c47f46f2d6e347fbdd713e50
aulia <miqbalar16@gmail.com>
2018-08-13 10:49:24 +0700

    Menambah Produk

--- product.php
+++ product.php
@@ -8,6 +8,24 @@
 </head>
 <body>
 
+<?php
 
+$products = ['Nike', 'Adidas', 'Puma', 'Reebok'];
+
+function set_product(&$data, $nama) {
+   array_push($data, $nama);
+}
+
+function get_products($data)
+{
+   foreach ($data as $product) {
+       echo $product . ' ';
+   }
+}
+
+set_product($products, 'Airwalk');
+get_products($products);
+
+?>
 </body>
 </html>
