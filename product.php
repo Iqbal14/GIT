0c5c3db598f67725cc0eacca1c47ba30285a3b46
aulia <miqbalar16@gmail.com>
2018-08-13 10:36:20 +0700

    Penambahann php product

--- product.php
+++ product.php
@@ -7,20 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-<?php
 
-function getProducts()
-{
-    $products = ['Nike', 'Adidas', 'Puma', 'Reebok'];
-
-    foreach ($products as $product) {
-        echo $product . ' ';
-    }
-}
-
-getProducts();
-
-?>
 
 </body>
 </html>
