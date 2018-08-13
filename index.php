0f47460bec7214edb6f06a06442d85d91f63f4e1
aulia <miqbalar16@gmail.com>
2018-08-13 10:55:20 +0700

    Menambah index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+    <ul>
+        <li>product 1</li>
+        <li>product 2</li>
+        <li>product 3</li>
+        <li>product 4</li>
+        <li>product 5</li>
+    </ul>
+</nav>
+</body>
+</html>
